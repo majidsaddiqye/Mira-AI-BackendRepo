1f85b85aa37f7cc86b379656bdc5d0879b7f3135
Abdul Majid <majidsaddiqye@gmail.com>
2025-12-01 01:39:57 +0500

    integrate Socketio

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connectDB = require('./src/db/db')
-
-
-
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
+const initSocketServer = require("./src/sockets/socket.server");
+const httpServer = require("http").createServer(app);
 
 // Connect Database
-connectDB()
+connectDB();
+
+//Socketio Server
+initSocketServer(httpServer);
 
 //Server Start
-const PORT= process.env.PORT
-app.listen(PORT,()=>{
-    console.log(`Server is Listening on port ${PORT}`)
-})
+const PORT = process.env.PORT;
+httpServer.listen(PORT, () => {
+  console.log(`Server is Listening on port ${PORT}`);
+});
